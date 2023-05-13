22f6d6d9866ef0a53661b94ed0fe224a2b2e4359
Howard Hinestroza <50192104+howardhr@users.noreply.github.com>
2023-05-13 13:15:51 -0500

    Creacion de user

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/howardhr/goms_user
+
+go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/howardhr/goms_domain v0.0.0-20230513052512-59cc802b4c7b // indirect
+	github.com/howardhr/goms_meta v0.0.0-20230513173321-606edcfa45fa // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ user/endpoint.go
@@ -0,0 +1,177 @@
+package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/howardhr/goms_meta/meta"
+)
+
+type (
+	Controller func(w http.ResponseWriter, r *http.Request)
+
+	Endpoints struct {
+		Create Controller
+		Get    Controller
+		GetAll Controller
+		Update Controller
+		Delete Controller
+	}
+
+	CreateReq struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Email     string `json:"email"`
+		Phone     string `json:"phone"`
+	}
+
+	Response struct {
+		Status int         `json:"status"`
+		Data   interface{} `json:"data,omitempty"`
+		Err    string      `json:"error,omitempty"`
+		Meta   *meta.Meta  `json:"meta,omitempty"`
+	}
+
+	UpdateReq struct {
+		FirstName *string `json:"first_name"`
+		LastName  *string `json:"last_name"`
+		Email     *string `json:"email"`
+		Phone     *string `json:"phone"`
+	}
+
+	Config struct {
+		LimitPageDef string
+	}
+)
+
+func MakeEndpoints(s Service, config Config) Endpoints {
+	return Endpoints{
+		Create: makeCreateEndpoint(s),
+		Get:    makeGetEndpoint(s),
+		GetAll: makeGetAllEndpoint(s, config),
+		Update: makeUpdateEndpoint(s),
+		Delete: makeDeleteEndpoint(s),
+	}
+}
+
+func makeCreateEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req CreateReq
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "Formato Invalido"})
+			return
+		}
+		if len(req.FirstName) < 1 {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "El nombre es requerido"})
+			return
+		}
+		if len(req.LastName) < 1 {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "El apellido es requerido"})
+			return
+		}
+		user, err := s.Create(req.FirstName, req.LastName, req.Email, req.Phone)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: err.Error()})
+			return
+		}
+		json.NewEncoder(w).Encode(&Response{Status: 200, Data: user})
+	}
+}
+func makeGetAllEndpoint(s Service, config Config) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		v := r.URL.Query()
+
+		filters := Filters{
+			v.Get("first_name"),
+			v.Get("last_name"),
+		}
+
+		limit, _ := strconv.Atoi(v.Get("limit"))
+		page, _ := strconv.Atoi(v.Get("page"))
+		count, err := s.Count(filters)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: err.Error()})
+			return
+		}
+
+		meta, err := meta.New(page, limit, count, config.LimitPageDef)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: err.Error()})
+			return
+		}
+
+		users, err := s.GetAll(filters, meta.Offset(), meta.Limit())
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: err.Error()})
+			return
+		}
+
+		json.NewEncoder(w).Encode(&Response{Status: 200, Data: users, Meta: meta})
+	}
+}
+func makeGetEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := mux.Vars(r)
+		id := path["id"]
+		fmt.Println("id que envia", id)
+		user, err := s.Get(id)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "El usuario no existe"})
+			return
+		}
+		json.NewEncoder(w).Encode(user)
+	}
+}
+func makeUpdateEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req UpdateReq
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "Invalid request format"})
+			return
+		}
+		if req.FirstName != nil && *req.FirstName == "" {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "El nombre es requerido"})
+			return
+		}
+
+		if req.LastName != nil && *req.LastName == "" {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "el apellido es requerido"})
+			return
+		}
+		path := mux.Vars(r)
+		id := path["id"]
+		if err := s.Update(id, req.FirstName, req.LastName, req.Email, req.Phone); err != nil {
+			w.WriteHeader(404)
+			json.NewEncoder(w).Encode(&Response{Status: 400, Err: "El usuario no existe"})
+			return
+		}
+		json.NewEncoder(w).Encode(map[string]string{"data": "success"})
+	}
+}
+func makeDeleteEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := mux.Vars(r)
+		id := path["id"]
+		if err := s.Delete(id); err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+			return
+		}
+		json.NewEncoder(w).Encode(map[string]string{"ok": "success"})
+	}
+
+}
